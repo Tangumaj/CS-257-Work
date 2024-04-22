c949d5bb67cbeee84f716bdf123b8564763425e3
julian tanguma <Tangumaj@carleton.edu>
2024-04-22 18:33:57 -0500

    edit

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -18,5 +18,5 @@ row = cur.fetchone()
 if row == None:
     print("Not found, please look for another city")
 else:
-    print( row[3] ) 
-    print( row[4] ) 
+    print( "Lat: %d", row[3] ) 
+    print( "Lon: %d", row[4] ) 
