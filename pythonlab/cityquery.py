9874166c253ac96e511e971e5f4f39f02e436024
julian tanguma <Tangumaj@carleton.edu>
2024-04-22 17:20:24 -0500

    edit

---
+++ pythonlab/cityquery.py
@@ -0,0 +1,12 @@
+conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    database="tangumaj",
+    user="tangumaj",
+    password="ardi363puppy")
+    
+cur = conn.cursor()
+
+sql = "SELECT Northfield FROM cities"
+    
+cur.execute( sql )
