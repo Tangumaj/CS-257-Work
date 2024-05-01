050643697920330ca30c84205ea48fe71383d958
Tangumaj <165401057+Tangumaj@users.noreply.github.com>
2024-05-01 10:23:24 -0500

    Update goth.py

--- flasklab/goth.py
+++ flasklab/goth.py
@@ -8,15 +8,15 @@ app = Flask(__name__)
 def welcome():
     return render_template("index.html")
 
-@app.route('/rand/<low>/<high>')
-def rand(low, high):
-    #Input values that come from a URL (i.e., @app.route)
-    #   are always strings so I need to convert the type to int
-    low_int = int(low)
-    high_int = int(high)
+# @app.route('/rand/<low>/<high>')
+# def rand(low, high):
+#     #Input values that come from a URL (i.e., @app.route)
+#     #   are always strings so I need to convert the type to int
+#     low_int = int(low)
+#     high_int = int(high)
     
-    num = random.randint(low_int, high_int)
-    return render_template("random.html", randNum = num)
+#     num = random.randint(low_int, high_int)
+#     return render_template("random.html", randNum = num)
 
 if __name__ == '__main__':
     my_port = 5227
